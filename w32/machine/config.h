5186e79d896f34213ea7389135a0ed540e42171c
shigio <shigio>
2000-08-26 17:16:18 +0000

    Initial revision

---
+++ w32/machine/config.h
@@ -0,0 +1,133 @@
+/* machine/config.h.  Generated automatically by configure.  */
+/* machine/config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define to empty if the keyword does not work.  */
+/* #undef const */
+
+/* Define if your struct stat has st_blksize.  */
+/* #define HAVE_ST_BLKSIZE 1 */
+
+/* Define if you have the strftime function.  */
+#define HAVE_STRFTIME 1
+
+/* Define to `long' if <sys/types.h> doesn't define.  */
+/* #undef off_t */
+
+/* Define as the return type of signal handlers (int or void).  */
+#define RETSIGTYPE void
+
+/* Define to `unsigned' if <sys/types.h> doesn't define.  */
+/* #undef size_t */
+
+/* Define if the `S_IS*' macros in <sys/stat.h> do not work properly.  */
+/* #undef STAT_MACROS_BROKEN */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if your processor stores words with the most significant
+   byte first (like Motorola and SPARC, unlike Intel and VAX).  */
+/* #undef WORDS_BIGENDIAN */
+
+/* Define to `default' if you have type */
+/* #undef int32_t */
+
+/* Define to `default' if you have type */
+/* #undef int8_t */
+
+/* Define to `default' if you have type */
+/* #undef int16_t */
+
+/* Define to `default' if you have type */
+/* #undef int32_t */
+
+/* Define to `default' if you have type */
+/* #undef u_int8_t */
+
+/* Define to `default' if you have type */
+/* #undef u_int16_t */
+
+/* Define to `default' if you have type */
+/* #undef u_int32_t */
+
+/* Define to `default' if you have type */
+/* #undef size_t */
+
+/* Define to `default' if you have type */
+/* #undef ssize_t */
+
+/* Define to `default' if you have type */
+/* #undef caddr_t */
+
+/* Define if your struct dirent has dp_d_namlen.  */
+/* #define HAVE_DP_D_NAMLEN 1 */
+
+/* Define if your struct dirent has dp_d_type.  */
+/* #define HAVE_DP_D_TYPE 1 */
+
+/* Define if your system have find(1). */
+/* #define HAVE_FIND 1 */
+
+/* Define if your system have xargs(1). */
+/* #define HAVE_XARGS 1 */
+
+/* Define if your system have grep(1). */
+/* #define HAVE_GREP 1 */
+
+/* Define for current machine architecture. */
+/* #undef WORDS_BIGENDIAN */
+
+/* The number of bytes in a char.  */
+#define SIZEOF_CHAR 1
+
+/* The number of bytes in a int.  */
+#define SIZEOF_INT 4
+
+/* The number of bytes in a short.  */
+#define SIZEOF_SHORT 2
+
+/* Define if you have the getcwd function.  */
+#define HAVE_GETCWD 1
+
+/* Define if you have the lstat function.  */
+/* #define HAVE_LSTAT 1 */
+
+/* Define if you have the putenv function.  */
+#define HAVE_PUTENV 1
+
+/* Define if you have the snprintf function.  */
+#define HAVE_SNPRINTF 1
+
+/* Define if you have the strdup function.  */
+#define HAVE_STRDUP 1
+
+/* Define if you have the <dirent.h> header file.  */
+#define HAVE_DIRENT_H 1
+
+/* Define if you have the <limits.h> header file.  */
+#define HAVE_LIMITS_H 1
+
+/* Define if you have the <ndir.h> header file.  */
+/* #undef HAVE_NDIR_H */
+
+/* Define if you have the <stdarg.h> header file.  */
+#define HAVE_STDARG_H 1
+
+/* Define if you have the <string.h> header file.  */
+#define HAVE_STRING_H 1
+
+/* Define if you have the <sys/dir.h> header file.  */
+/* #undef HAVE_SYS_DIR_H */
+
+/* Define if you have the <sys/ndir.h> header file.  */
+/* #undef HAVE_SYS_NDIR_H */
+
+/* Define if you have the <unistd.h> header file.  */
+#define HAVE_UNISTD_H 1
+
+/* Name of package */
+#define PACKAGE "global"
+
+/* Version number of package */
+#define VERSION "4.0"
+
