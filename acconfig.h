32d5b70fcaaa17fe18e64582778078c6633a39ed
shigio <shigio>
2000-09-26 07:09:43 +0000

    Initial revision

---
+++ acconfig.h
@@ -0,0 +1,62 @@
+/* Define to `default' if you have type */
+#undef int32_t
+
+/* Define to `default' if you have type */
+#undef u_char
+
+/* Define to `default' if you have type */
+#undef u_int
+
+/* Define to `default' if you have type */
+#undef u_long
+
+/* Define to `default' if you have type */
+#undef u_short
+
+/* Define to `default' if you have type */
+#undef int8_t
+
+/* Define to `default' if you have type */
+#undef int16_t
+
+/* Define to `default' if you have type */
+#undef int32_t
+
+/* Define to `default' if you have type */
+#undef u_int8_t
+
+/* Define to `default' if you have type */
+#undef u_int16_t
+
+/* Define to `default' if you have type */
+#undef u_int32_t
+
+/* Define to `default' if you have type */
+#undef size_t
+
+/* Define to `default' if you have type */
+#undef ssize_t
+
+/* Define to `default' if you have type */
+#undef caddr_t
+
+/* Define to `default' if you have type */
+#undef sigset_t
+
+/* Define if your struct dirent has dp_d_namlen.  */
+#undef HAVE_DP_D_NAMLEN
+
+/* Define if your struct dirent has dp_d_type.  */
+#undef HAVE_DP_D_TYPE
+
+/* Define if your system have find(1). */
+#undef HAVE_FIND
+
+/* Define if your system have xargs(1). */
+#undef HAVE_XARGS
+
+/* Define if your system have grep(1). */
+#undef HAVE_GREP
+
+/* Define for current machine architecture. */
+#undef WORDS_BIGENDIAN
