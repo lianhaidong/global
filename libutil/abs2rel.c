ebd7f25c1d715c888ef6248668530dfb679706de
shigio <shigio>
2002-02-14 17:14:03 +0000

    path convertion method replaced.

---
+++ libutil/abs2rel.c
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 1997
+ *             Shigio Yamaguchi. All rights reserved.
+ * Copyright (c) 1999
+ *             Tama Communications Corporation. All rights reserved.
+ *
+ * This file is part of GNU GLOBAL.
+ *
+ * GNU GLOBAL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * GNU GLOBAL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+#include <errno.h>
+#ifdef STDC_HEADERS
+#include <stdlib.h>
+#endif
+#ifdef HAVE_STRING_H
+#include <string.h>
+#else
+#include <strings.h>
+#endif
+
+#include "abs2rel.h"
+
+/*
+ * abs2rel: convert an absolute path name into relative.
+ *
+ *	i)	path	absolute path
+ *	i)	base	base directory (must be absolute path)
+ *	o)	result	result buffer
+ *	i)	size	size of result buffer
+ *	r)		!= NULL: relative path
+ *			== NULL: error
+ */
+char *
+abs2rel(path, base, result, size)
+	const char *path;
+	const char *base;
+	char *result;
+	const int size;
+{
+	const char *pp, *bp, *branch;
+	/*
+	 * endp points the last position which is safe in the result buffer.
+	 */
+	const char *endp = result + size - 1;
+	char *rp;
+
+	if (*path != '/') {
+		if (strlen(path) >= size)
+			goto erange;
+		strcpy(result, path);
+		goto finish;
+	} else if (*base != '/' || !size) {
+		errno = EINVAL;
+		return (NULL);
+	} else if (size == 1)
+		goto erange;
+	/*
+	 * seek to branched point.
+	 */
+	branch = path;
+	for (pp = path, bp = base; *pp && *bp && *pp == *bp; pp++, bp++)
+		if (*pp == '/')
+			branch = pp;
+	if ((*pp == 0 || *pp == '/' && *(pp + 1) == 0) &&
+	    (*bp == 0 || *bp == '/' && *(bp + 1) == 0)) {
+		rp = result;
+		*rp++ = '.';
+		if (*pp == '/' || *(pp - 1) == '/')
+			*rp++ = '/';
+		if (rp > endp)
+			goto erange;
+		*rp = 0;
+		goto finish;
+	}
+	if (*pp == 0 && *bp == '/' || *pp == '/' && *bp == 0)
+		branch = pp;
+	/*
+	 * up to root.
+	 */
+	rp = result;
+	for (bp = base + (branch - path); *bp; bp++)
+		if (*bp == '/' && *(bp + 1) != 0) {
+			if (rp + 3 > endp)
+				goto erange;
+			*rp++ = '.';
+			*rp++ = '.';
+			*rp++ = '/';
+		}
+	if (rp > endp)
+		goto erange;
+	*rp = 0;
+	/*
+	 * down to leaf.
+	 */
+	if (*branch) {
+		if (rp + strlen(branch + 1) > endp)
+			goto erange;
+		strcpy(rp, branch + 1);
+	} else
+		*--rp = 0;
+finish:
+	return result;
+erange:
+	errno = ERANGE;
+	return (NULL);
+}
---
+++ libutil/abs2rel.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2002
+ *             Tama Communications Corporation. All rights reserved.
+ *
+ * This file is part of GNU GLOBAL.
+ *
+ * GNU GLOBAL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * GNU GLOBAL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef _RELATIVE_H_
+#define _RELATIVE_H_
+
+char	*abs2rel(const char *, const char *, char *, const int);
+
+#endif /* ! _RELATIVE_H_ */
