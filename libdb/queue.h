0004f99fc7964807cd3649f1662cabed01bf0eb6
shigio <shigio>
2002-10-28 00:50:10 +0000

    #include <machine/ansi.h> removed for compatibility.

--- libdb/queue.h
+++ libdb/queue.h
@@ -37,7 +37,7 @@
 #ifndef _SYS_QUEUE_H_
 #define	_SYS_QUEUE_H_
 
-#include <machine/ansi.h>	/* for __offsetof */
+/* #include <machine/ansi.h>	/* for __offsetof */
 
 /*
  * This file defines five types of data structures: singly-linked lists,
