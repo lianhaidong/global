a30f5cf640b253325eef7bf9f403366dc47571b8
shigio <shigio>
2003-03-19 05:08:30 +0000

    * C.h,Cpp.h,java.h: rewrite using enum. * C.c,C.h: add C99 keywords _Bool,_Complex,inline,restrict. * Cpp.c,Cpp.h: add missing C++ keywords bool,const_cast,dynamic_cast,    export,false,reinterpret_cast,static_cast,true,typeid,wchar_t.

--- gctags/C.h
+++ gctags/C.h
@@ -23,69 +23,81 @@
 #define RULES		1
 #define PROGRAMS	2
 
-#define C___P		1001
-#define C___ATTRIBUTE__	1002
-#define C_ASM		1003
-#define C_AUTO		1004
-#define C_BREAK		1005
-#define C_CASE		1006
-#define C_CHAR		1007
-#define C_CONST		1008
-#define C_CONTINUE	1009
-#define C_DEFAULT	1010
-#define C_DO		1011
-#define C_DOUBLE	1012
-#define C_ELSE		1013
-#define C_ENUM		1014
-#define C_EXTERN	1015
-#define C_FLOAT		1016
-#define C_FOR		1017
-#define C_GOTO		1018
-#define C_IF		1019
-#define C_INT		1020
-#define C_LONG		1021
-#define C_REGISTER	1022
-#define C_RETURN	1023
-#define C_SHORT		1024
-#define C_SIGNED	1025
-#define C_SIZEOF	1026
-#define C_STATIC	1027
-#define C_STRUCT	1028
-#define C_SWITCH	1029
-#define C_TYPEDEF	1030
-#define C_UNION		1031
-#define C_UNSIGNED	1032
-#define C_VOID		1033
-#define C_VOLATILE	1034
-#define C_WHILE		1035
-#define CP_ELIF		2001
-#define CP_ELSE		2002
-#define CP_DEFINE	2003
-#define CP_IF		2004
-#define CP_IFDEF	2005
-#define CP_IFNDEF	2006
-#define CP_INCLUDE	2007
-#define CP_PRAGMA	2008
-#define CP_SHARP	2009
-#define CP_ERROR	2010
-#define CP_UNDEF	2011
-#define CP_ENDIF	2012
-#define CP_LINE		2013
-#define CP_WARNING	2014
-#define CP_IDENT	2015
-#define CP_ASSERT	2016
-#define YACC_SEP	3001
-#define YACC_BEGIN	3002
-#define YACC_END	3003
-#define YACC_LEFT	3004
-#define YACC_NONASSOC	3005
-#define YACC_RIGHT	3006
-#define YACC_START	3007
-#define YACC_TOKEN	3008
-#define YACC_TYPE	3009
-#define YACC_UNION	3010
-#define YACC_TERM	3011
+enum {
+	CTOKEN_BASE = 1000,
+	C___P,
+	C___ATTRIBUTE__,
+	C__BOOL,
+	C__COMPLEX,
+	C__IMAGINARY,
+	C_ASM,
+	C_AUTO,
+	C_BREAK,
+	C_CASE,
+	C_CHAR,
+	C_CONST,
+	C_CONTINUE,
+	C_DEFAULT,
+	C_DO,
+	C_DOUBLE,
+	C_ELSE,
+	C_ENUM,
+	C_EXTERN,
+	C_FLOAT,
+	C_FOR,
+	C_GOTO,
+	C_IF,
+	C_INT,
+	C_INLINE,
+	C_LONG,
+	C_REGISTER,
+	C_RESTRICT,
+	C_RETURN,
+	C_SHORT,
+	C_SIGNED,
+	C_SIZEOF,
+	C_STATIC,
+	C_STRUCT,
+	C_SWITCH,
+	C_TYPEDEF,
+	C_UNION,
+	C_UNSIGNED,
+	C_VOID,
+	C_VOLATILE,
+	C_WHILE,
 
-#define IS_CTOKEN(c)	(c > 1000 && c < 2001)
-#define IS_CPTOKEN(c)	(c > 2000 && c < 3001)
-#define IS_YACCTOKEN(c)	(c > 3000 && c < 4001)
+	CPTOKEN_BASE = 2000,
+	CP_ASSERT,
+	CP_DEFINE,
+	CP_ELIF,
+	CP_ELSE,
+	CP_ENDIF,
+	CP_ERROR,
+	CP_IDENT,
+	CP_IF,
+	CP_IFDEF,
+	CP_IFNDEF,
+	CP_INCLUDE,
+	CP_LINE,
+	CP_PRAGMA,
+	CP_SHARP,
+	CP_UNDEF,
+	CP_WARNING,
+
+	YACCTOKEN_BASE = 3000,
+	YACC_SEP,
+	YACC_BEGIN,
+	YACC_END,
+	YACC_LEFT,
+	YACC_NONASSOC,
+	YACC_RIGHT,
+	YACC_START,
+	YACC_TOKEN,
+	YACC_TYPE,
+	YACC_UNION,
+	YACC_TERM
+};
+
+#define IS_CTOKEN(c)	((c) > CTOKEN_BASE && (c) < CTOKEN_BASE + 1000)
+#define IS_CPTOKEN(c)	((c) > CPTOKEN_BASE && (c) < CPTOKEN_BASE + 1000)
+#define IS_YACCTOKEN(c)	((c) > YACCTOKEN_BASE && (c) < YACCTOKEN_BASE + 1000)
--- gctags/java.h
+++ gctags/java.h
@@ -22,59 +22,62 @@
 /*
  * java's reserved words.
  */
-#define	J_ABSTRACT	1001
-#define	J_BOOLEAN	1002
-#define	J_BREAK		1003
-#define	J_BYTE		1004
-#define	J_CASE		1005
-#define	J_CATCH		1006
-#define	J_CHAR		1007
-#define	J_CLASS		1008
-#define	J_CONST		1009
-#define	J_CONTINUE	1010
-#define	J_DEFAULT	1011
-#define	J_DO		1012
-#define	J_DOUBLE	1013
-#define	J_ELSE		1014
-#define	J_EXTENDS	1015
-#define	J_FALSE		1016
-#define	J_FINAL		1017
-#define	J_FINALLY	1018
-#define	J_FLOAT		1019
-#define	J_FOR		1020
-#define	J_GOTO		1021
-#define	J_IF		1022
-#define	J_IMPLEMENTS	1023
-#define	J_IMPORT	1024
-#define	J_INSTANCEOF	1025
-#define	J_INT		1026
-#define	J_INTERFACE	1027
-#define	J_LONG		1028
-#define	J_NATIVE	1029
-#define	J_NEW		1030
-#define	J_NULL		1031
-#define	J_PACKAGE	1032
-#define	J_PRIVATE	1033
-#define	J_PROTECTED	1034
-#define	J_PUBLIC	1035
-#define	J_RETURN	1036
-#define	J_SHORT		1037
-#define	J_STATIC	1038
-#define	J_SUPER		1039
-#define	J_SWITCH	1040
-#define	J_SYNCHRONIZED	1041
-#define	J_THIS		1042
-#define	J_THROW		1043
-#define	J_THROWS	1044
-#define	J_UNION		1045
-#define	J_TRANSIENT	1046
-#define	J_TRUE		1047
-#define	J_TRY		1048
-#define	J_VOID		1049
-#define	J_VOLATILE	1050
-#define	J_WHILE		1051
-#define	J_STRICTFP	1052
-#define	J_WIDEFP	1053
+enum {
+	JTOKEN_BASE = 1000,
+	J_ABSTRACT,
+	J_BOOLEAN,
+	J_BREAK,
+	J_BYTE,
+	J_CASE,
+	J_CATCH,
+	J_CHAR,
+	J_CLASS,
+	J_CONST,
+	J_CONTINUE,
+	J_DEFAULT,
+	J_DO,
+	J_DOUBLE,
+	J_ELSE,
+	J_EXTENDS,
+	J_FALSE,
+	J_FINAL,
+	J_FINALLY,
+	J_FLOAT,
+	J_FOR,
+	J_GOTO,
+	J_IF,
+	J_IMPLEMENTS,
+	J_IMPORT,
+	J_INSTANCEOF,
+	J_INT,
+	J_INTERFACE,
+	J_LONG,
+	J_NATIVE,
+	J_NEW,
+	J_NULL,
+	J_PACKAGE,
+	J_PRIVATE,
+	J_PROTECTED,
+	J_PUBLIC,
+	J_RETURN,
+	J_SHORT,
+	J_STATIC,
+	J_SUPER,
+	J_SWITCH,
+	J_SYNCHRONIZED,
+	J_THIS,
+	J_THROW,
+	J_THROWS,
+	J_UNION,
+	J_TRANSIENT,
+	J_TRUE,
+	J_TRY,
+	J_VOID,
+	J_VOLATILE,
+	J_WHILE,
+	J_STRICTFP,
+	J_WIDEFP
+};
 
 #define MAXCOMPLETENAME 1024            /* max size of complete name of class */
 #define MAXCLASSSTACK   100             /* max size of class stack */
