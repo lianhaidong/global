d39162e7a4226a564355a9db50c429286ccf642a
shigio <shigio>
2004-02-01 10:13:01 +0000

    GLOBAL-4.7 released.

--- libutil/version.c
+++ libutil/version.c
@@ -30,7 +30,7 @@
 
 const char *copy = "\
 Copyright (c) 1996, 1997, 1998, 1999 Shigio Yamaguchi\n\
-Copyright (c) 1999, 2000, 2001, 2002, 2003 Tama Communications Corporation\n\
+Copyright (c) 1999, 2000, 2001, 2002, 2003, 2004 Tama Communications Corporation\n\
 GNU GLOBAL comes with NO WARRANTY,\n\
 to the extent permitted by law.\n\
 You may redistribute copies of GNU GLOBAL\n\
