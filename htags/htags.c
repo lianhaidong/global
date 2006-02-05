d6216bc6b3d7ee5bb0f981cad564f436540c39a3
h-iwamoto <h-iwamoto>
2006-02-05 07:24:32 +0000

    Since external sort command is no longer used in htags, it is not necessary to set LC_ALL.

--- htags/htags.c
+++ htags/htags.c
@@ -1677,11 +1677,6 @@ main(int argc, char **argv)
 	set_env("GTAGSDBPATH", dbpath);
 	set_env("GTAGSLIBPATH", "");
 	/*
-	 * Use C locale in order to avoid the degradation of performance
-	 * by internationalized sort command.
-	 */
-	set_env("LC_ALL", "C");
-	/*
 	 * check directories
 	 */
 	if (fflag || cflag || dynamic) {
