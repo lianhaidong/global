b4a55d37bc47a2f02fa1e5d437ddc6b7183b96ea
shigio <shigio>
2006-12-15 02:28:15 +0000

    Added copyright year.

--- libutil/dbop.c
+++ libutil/dbop.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2005
+ * Copyright (c) 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2005, 2006
  *	Tama Communications Corporation
  *
  * This file is part of GNU GLOBAL.
