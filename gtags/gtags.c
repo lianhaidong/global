541c9163d6ed6e49f52725ba9ee0882f4ccd5ca1
shigio <shigio>
2010-10-18 10:31:03 +0000

    Changed the -I command to accept GTAGSDBPATH and obj directory.

--- gtags/gtags.c
+++ gtags/gtags.c
@@ -467,6 +467,7 @@ main(int argc, char **argv)
 		strbuf_puts(sb, "mkid");
 		if (vflag)
 			strbuf_puts(sb, " -v");
+		strbuf_sprintf(sb, " --file='%s/ID'", dbpath);
 		if (vflag) {
 #ifdef __DJGPP__
 			if (is_unixy())	/* test for 4DOS as well? */
