5c35db8243e0f9ffa4c83c0796245c42c39e75a2
shigio <shigio>
2013-11-18 04:27:46 +0000

    Canceled a mistaken deletion.

--- htags/htags.c
+++ htags/htags.c
@@ -22,8 +22,21 @@
 #include <config.h>
 #endif
 #include <stdio.h>
+#ifdef STDC_HEADERS
+#include <stdlib.h>
+#endif
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+#ifdef HAVE_FCNTL_H
+#include <fcntl.h>
+#else
+#include <sys/file.h>
+#endif
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/param.h>
 #include <errno.h>
 
 #include "checkalloc.h"
