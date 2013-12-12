b95dfc79efd5dce3021f8a425f2a9505cac77931
shigio <shigio>
2013-12-12 09:23:06 +0000

    Added some comments.

--- plugin-factory/user-custom.c
+++ plugin-factory/user-custom.c
@@ -1,7 +1,15 @@
 #include "parser.h"
 
+/*
+ * NOP PARSER
+ *
+ * This is a skeleton parser. Nothing has been implemented yet.
+ * You can invoke this parser by the following command.
+ *
+ * $ gtags --gtagsconf=/usr/local/share/gtags/gtags.conf --gtagslabel=user
+ */
 void
 parser(const struct parser_param *param)
 {
-	param->warning("This is a skeleton function. Nothing has been implemented yet.");
+	param->warning("nop parser: %s", param->file);
 }
