fdbc592350fc1489246165c208f3252360ca2496
shigio <shigio>
2016-06-30 06:02:34 +0000

    Added an error message for some platforms which don't have PATH_MAX, most notably GNU/Hurd.

--- libutil/find.c
+++ libutil/find.c
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 1997, 1998, 1999, 2000, 2001, 2002, 2005, 2006, 2008,
- *	2009, 2011, 2012, 2014, 2015 Tama Communications Corporation
+ *	2009, 2011, 2012, 2014, 2015, 2016
+ * Tama Communications Corporation
  *
  * This file is part of GNU GLOBAL.
  *
@@ -78,7 +79,11 @@
 #define STRNCMP strncmp
 #define ROOT 0
 #endif
-
+#ifndef PATH_MAX
+#error Since this platform does not have PATH_MAX, you should define it
+#error using an appropriate value for the platform.
+/* #define PATH_MAX     1024 */
+#endif
 /*
  * usage of find_xxx()
  *
