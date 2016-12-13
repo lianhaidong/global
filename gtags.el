8377370d65d8b61c62aa0ca712e9ab1a1918f7ec
shigio <shigio>
2016-12-13 09:12:06 +0000

    Fixed warning: In gtags-select-it: gtags.el:877:13:Warning: `goto-line' is for interactive use only; use     `forward-line' instead.
    Patch by Punit Agrawal.

--- gtags.el
+++ gtags.el
@@ -875,7 +875,8 @@
 	    (find-file-other-window file)))
         (if delete (kill-buffer prev-buffer)))
       (setq gtags-current-buffer (current-buffer))
-      (goto-line line)
+      (goto-char (point-min))
+      (forward-line (1- line))
       (gtags-mode 1))))
 
 ;; make complete list (do nothing)
