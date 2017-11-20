6e6a3d0de3be0ea3cd6ee394afea334b1a9085b7
shigio <shigio>
2017-11-20 05:12:28 +0000

    Slightly modified. o Addedd label parameter. o Removed the last escape character.

--- plugin-factory/maps2conf.pl
+++ plugin-factory/maps2conf.pl
@@ -2,21 +2,26 @@
 #
 # maps2conf.pl: Generates langmaps which is suitable for gtags.conf.
 #
-# [usage] maps2conf.pl <ctags-path> <plugin-path>
+# [usage] maps2conf.pl <label> <ctags-path> <plugin-path> [<label>]
 #
 # $ maps2conf.pl /usr/local/bin/ctags '$libdir/gtags/universal-ctags.la'
 #
 my $count = $#ARGV + 1;
 if ($count < 2) {
-	die("usage: maps2conf.pl ctags-path plugin-path\n");
+	die("usage: maps2conf.pl ctags-path plugin-path [label]\n");
 }
 my $ctags = $ARGV[0];
 my $plugin = $ARGV[1];
+my $label = $ARGV[2];
+if ($label eq '') {
+	$label = 'default';
+}
 open(IN, "-|") || exec "$ctags", '--list-maps';
 if ($?) {
 	die("ctags --list-maps failed.\n");
 }
 my $map = $parser = '';
+$map = "$label:\\\n";
 $map .= "\t:ctagscom=$ctags:\\\n";
 $map .= "\t:ctagslib=$plugin:\\\n";
 while (<IN>) {
@@ -41,6 +46,11 @@ while (<IN>) {
 	}
 	$map .= ":\\\n";
 	$parser .= "\t:gtags_parser=$language\\:\$ctagslib:\\\n";
+}
+if (length($parser) > 1) {
+	chop($parser);
+	chop($parser);
+	$parser .= "\n";
 }
 close(IN);
 print $map;
